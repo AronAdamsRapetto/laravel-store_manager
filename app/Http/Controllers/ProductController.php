19f0bad66ea5374e1bd0d770bfa9a5e485b1b72d
Aron Adams <aronrapetto@hotmail.com>
2023-02-15 11:40:20 -0300

    feat: controller index and show

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = DB::select("select * from products");
+        return response()->json($products, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $product = DB::select('select * from products where id = ?', [$id]);
+
+        if (count($product) == 0) {
+            return response()->json(["message" => "Product not found"], 404);
+        }
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
